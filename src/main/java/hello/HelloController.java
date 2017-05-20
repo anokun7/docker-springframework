003144e8add07ef66afe471068cdf82f340f391a
Anoop V Kumar <anoop@docker.com>
2017-05-20 02:57:52 -0400

    Simple spring boot app to show scaling of services

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+//    @Value("#{environment['HOSTNAME']}")
+//    private String hostName;
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello from " + System.getenv("HOSTNAME") + "<hr/>";
+    }
+}
